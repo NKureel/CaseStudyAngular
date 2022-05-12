1c3d7b7dabddc979d266c0db96b5837c287d2de2
unknown <cogdotnet07@iiht.tech>
2022-05-13 04:31:20 +0530

    Update

--- FlightManagementUI/src/app/app.module.ts
+++ FlightManagementUI/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,    
-    LoginComponent,
-    RegisterComponent,
     HeaderComponent,
-    FooterComponent,
-    HomeComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
