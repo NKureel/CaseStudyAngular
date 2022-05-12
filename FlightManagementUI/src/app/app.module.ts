301281af4450c915eb696a1641029b27950c88f0
test <naina.kureel@cognizant.com>
2022-05-12 18:32:09 +0530

    Update

--- FlightManagementUI/src/app/app.module.ts
+++ FlightManagementUI/src/app/app.module.ts
@@ -3,22 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AirlineComponent } from './airline/airline.component';
-import { InventoryComponent } from './inventory/inventory.component';
-import { BookingComponent } from './booking/booking.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AirlineComponent,
-    InventoryComponent,
-    BookingComponent,
+    AppComponent,    
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
