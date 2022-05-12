5f43237b6c864d88a80ecb0fd693bf330b74e9f5
test <naina.kureel@cognizant.com>
2022-05-12 19:03:00 +0530

    Update

--- FlightManagementUI/src/app/app-routing.module.ts
+++ FlightManagementUI/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/Flight',
-    pathMatch: 'full'  
+    redirectTo: '/flight',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   },
   {
     path: 'login',
@@ -16,7 +21,7 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
-  }  
+  }
 ];
 
 @NgModule({
