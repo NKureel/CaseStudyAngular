86df9d155191d3b0a57fce4a8891c6a855b61d1f
test <naina.kureel@cognizant.com>
2022-05-16 20:58:23 +0530

    Update

--- FlightManagementUI/src/app/home/home.component.ts
+++ FlightManagementUI/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery'; 
 declare  var jQuery:  any;
 @Component({
   selector:'app-home',
@@ -8,9 +9,17 @@ declare  var jQuery:  any;
 export class HomeComponent implements OnInit { 
   constructor() { }
   //rowData = [];
-  
+ 
   ngOnInit(): void {
-
+    
+      $(document).ready(function () {
+       (<any> $('#dtDynamicVerticalScrollExample')).DataTable({
+          "scrollY": "50vh",
+          "scrollCollapse": true,
+        });
+        $('.dataTables_length').addClass('bs-select');
+      });
+    
     /*fetch('https://www.ag-grid.com/example-assets/row-data.json')
     .then(result => result.json())
     .then(rowData => this.rowData = rowData);*/      
